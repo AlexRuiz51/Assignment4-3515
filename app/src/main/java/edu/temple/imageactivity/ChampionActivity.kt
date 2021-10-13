caf8e8fb6c593a24956476d78927b1258dbe0c70
Alex Ruiz <46982327+AlexRuiz51@users.noreply.github.com>
2021-10-13 06:34:32 -0400

    ++INSTRUCTIONS ++NEW IMAGES ++ALL HARD CODED STRINGS REPLACED WITH STRINGS.XML ++NOW IN FRENCH --> NEW STRINGS.XML FILE

--- app/src/main/java/edu/temple/imageactivity/ChampionActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ChampionActivity.kt
@@ -10,10 +10,10 @@ class ChampionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_champion)
 
-        val intent: Intent = intent;
-        var image:Int = intent.getIntExtra("image", 0);
-        var name: String = "League of Legends Champion: " + intent.getStringExtra("name")!!;
-        imageView.setImageResource(image);
-        textView.setText(name)
+        val intent: Intent = intent
+        var image:Int = intent.getIntExtra("image", 0)
+        var name: String = resources.getString(R.string.championDescription) + intent.getStringExtra("name")!!
+        imageView.setImageResource(image)
+        textView.text = name
     }
 }
--- app/src/main/java/edu/temple/imageactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/MainActivity.kt
@@ -13,34 +13,34 @@ class MainActivity : AppCompatActivity() {
     }
     fun onClickListener(){
         cElise.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Elise").putExtra("image",R.drawable.elise))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championElise)).putExtra("image",R.drawable.elise))
         }
         cEvelynn.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Evelynn").putExtra("image",R.drawable.evelynn))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championEvelynn)).putExtra("image",R.drawable.evelynn))
         }
         cKayn.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Kayn").putExtra("image",R.drawable.kayn))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championKayn)).putExtra("image",R.drawable.kayn))
         }
         cKhazix.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Kha'Zix").putExtra("image",R.drawable.khazix))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championKhaZix)).putExtra("image",R.drawable.khazix))
         }
         cNunuWill.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Nunu & Willump").putExtra("image",R.drawable.nunuwill))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championNunuandWillump)).putExtra("image",R.drawable.nunuwill))
         }
         cRammus.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Rammus").putExtra("image",R.drawable.rammus))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championRammus)).putExtra("image",R.drawable.rammus))
         }
         cRengar.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Rengar").putExtra("image",R.drawable.rengar))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championRengar)).putExtra("image",R.drawable.rengar))
         }
         cShen.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Shen").putExtra("image",R.drawable.shen))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championShen)).putExtra("image",R.drawable.shen))
         }
         cSwain.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Swain").putExtra("image",R.drawable.swain))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championSwain)).putExtra("image",R.drawable.swain))
         }
         cWarwick.setOnClickListener{
-            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", "Warwick").putExtra("image",R.drawable.warwick))
+            startActivity(Intent(this, ChampionActivity::class.java).putExtra("name", resources.getString(R.string.championWarwick)).putExtra("image",R.drawable.warwick))
         }
     }
 }
