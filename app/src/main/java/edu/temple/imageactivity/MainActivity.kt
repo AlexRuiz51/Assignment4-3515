e4668d8c108675e3dd4dc1c89712fe29b317cb5f
Alex Ruiz <46982327+AlexRuiz51@users.noreply.github.com>
2021-10-13 03:51:17 -0400

    assignment 5 branch init

--- app/src/main/java/edu/temple/imageactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        onClickListener();
+        onClickListener()
     }
     fun onClickListener(){
         cElise.setOnClickListener{
